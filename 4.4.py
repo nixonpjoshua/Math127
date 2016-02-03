59ef11c529b8a53a390dbd05bfa0c14a820e90cd
Joshua P Nixon <nixonpjoshua@gmail.com>
2016-02-02 22:05:38 -0800

    fixed bugs, math problems ahead
    the code converges to .25 for all probabilities now, which seems
    correct, however the find_eq function returns a normalized unit vector
    so is .5 for all entries

--- 4.4.py
+++ 4.4.py
@@ -1,13 +1,14 @@
 import numpy as np
-a = .03, b = a/3
-M = np.array([[1-a, b, b, b], [b, 1-a, b, b], [b, b, 1-a, b]. [b, b, b, 1-a]])
+a = .03
+b = a/3
+M = np.array([[1-a, b, b, b], [b, 1-a, b, b], [b, b, 1-a, b], [b, b, b, 1-a]])
 p = np.array([.2, .3, .4, .1])
 
 
 def find_eq(M):
     D, V = np.linalg.eig(M)
     for x in xrange(D.size):
-        if D[x] == 1:
+        if abs(D[x] - 1) < .0001:
             return V[:, x]
 
 
@@ -15,12 +16,12 @@ def problem_443_a(epsilon, p_t, M):
     p_eq = find_eq(M)
     def is_within_epsilon(p_t):
         t = True
-        for i in xrange(p_0.size):
-            t = t and abs(p_eq - p_t[i]) < epsilon
+        for i in xrange(p_t.size):
+            t = t and abs(p_eq[i] - p_t[i]) < epsilon
         return t
     count = 0
-    while is_within_epsilon(p_t):
-        p_t = M*p_t
+    while not is_within_epsilon(p_t):
+        p_t = M.dot(p_t)
         count += 1
     return count
 small = 0
@@ -28,7 +29,7 @@ large = 0
 iter = 100
 
 for x in xrange(iter):
-    large += problem_443_a(0.05, p, p_eq, M)
-    small += problem_443_a(0.01, p, p_eq, M)
-print('average for .05' + large/iter)
-print('average for .01' + small/iter)
+    large += problem_443_a(0.05, p, M)
+    small += problem_443_a(0.01, p, M)
+print('average for .05' + str(large/iter))
+print('average for .01' + str(small/iter))
