c027f22adb06e8dd84ba8abc6e176e6d630b2f65
Joshua P Nixon <nixonpjoshua@gmail.com>
2016-02-06 13:53:48 -0800

    changed seq to be a list until returned

--- mutate.py
+++ mutate.py
@@ -7,8 +7,8 @@ def transition_matrix(a):
                  [b, b, 1-a, b],
                  [b, b, b, 1-a]])
     return M
-<<<<<<< Updated upstream
-def position_to_DNA(nuc):
+
+def DNA_to_position(nuc):
     if nuc == 'A':
         return 0
     if nuc == 'G':
@@ -17,7 +17,8 @@ def position_to_DNA(nuc):
         return 2
     if nuc == 'T':
         return 3
-def DNA_to_position(pos):
+
+def position_to_DNA(pos):
     if pos == 0:
         return 'A'
     if pos == 1:
@@ -25,52 +26,30 @@ def DNA_to_position(pos):
     if pos == 2:
         return 'C'
     if pos == 3:
-=======
-
-def position_to_DNA(nuc):
-    if  nuc == 'A':
-        return 0
-    if  nuc == 'G':
-        return 1
-    if  nuc == 'C':
-        return 2
-    if  nuc == 'T':
-        return 3
-
-def DNA_to_position(nuc):
-    if  nuc == 0:
-        return 'A'
-    if  nuc == 1:
-        return 'G'
-    if  nuc == 2:
-        return 'C'
-    if  nuc == 3:
->>>>>>> Stashed changes
         return 'T'
 """
     mutates the DNA string seq, int t times
     according to the Jukes-Cantor model specified by the alpha value, A
 """
 def mutate(a, t, seq):
+    seq = list(seq)
     A = transition_matrix(a)
     for i in xrange(len(seq)):
         nuc = seq[i]
         p = np.zeros(4)
         p[DNA_to_position(nuc)] = 1
-        p = np.transpose(p)
-        print(p)
         for i in xrange(t):
-            p = A*p
+            p = A.dot(p)
         seed = np.random.rand()
         if seed <= p[0]:
-            seq[i] = DNA_to_position(0)
+            seq[i] = position_to_DNA(0)
         elif seed <= p[0]+p[1]:
-            seq[i] = DNA_to_position(1)
+            seq[i] = position_to_DNA(1)
         elif seed <= p[0]+p[1]+p[2]:
-            seq[i] = DNA_to_position(2)
+            seq[i] = position_to_DNA(2)
         else:
-            seq[i] = DNA_to_position(3)
-    return seq
+            seq[i] = position_to_DNA(3)
+    return ''.join(seq)
 
 print(mutate(.3,4,'GATTACA'))
 
