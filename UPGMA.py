90184141fd96156578c2940df87d552798b762eb
Joshua P Nixon <nixonpjoshua@gmail.com>
2016-02-21 19:11:20 -0800

    removing jukes cantor from upgma

--- UPGMA.py
+++ UPGMA.py
@@ -5,86 +5,8 @@
 #          Bidit Acharya
 #          Tracy Lou
 
-import matplotlib.pyplot as plt
 import numpy as np
-import math
 from ete3 import Tree
-
-############# Defined for Testing ##############
-
-A = np.array([[5, 3, 2],
-              [1, 4, 5],
-              [6, 7, 8],
-             ])
-
-B = np.array([[5, 3, 9],
-              [3, 4, 1],
-              [6, 7, 8],
-             ])
-
-################################################
-"""
-Constructs a Jukes Cantor transition Matrix with a specified alpha level a
-Args:
-     a: alpha level for the Jukes Cantor Matrix
-Returns:
-     Transition Matrix corresponding to the Jukes-Cantor Algorithm
-"""
-
-def JC_matrix(a):
-
-    """
-    >>> np.trace(JC_matrix(.25))
-    3.0
-    """
-
-    b = a/3
-    M = np.array([[1-a, b, b, b],
-                 [b, 1-a, b, b],
-                 [b, b, 1-a, b],
-                 [b, b, b, 1-a]])
-    return M
-
-
-"""
-Computes proportion of differing letters from two strings of the same size
-Args:
-    s1: string 1
-    s2: string 2 
-Returns:
-    Throws error if the strings are not of the same length
-    Else, returns proportion (in between 0 and 1) of differing letters
-"""
-
-def prop_diff(s1,s2):
-    # """ LOOK curious as to why this isn't working
-    # >>> prop_diff("ATTGAC","ATGGCC") 
-    # float(2)/float(6)  
-    # """
-    if len(s1) != len(s2):
-        raise ValueError("Cannot compute compare DNA sequences of differing lenth")
-    diffs = 0
-    i     = 0
-    while i < len(s1):
-        if s1[i] != s2[i]:
-            diffs += 1
-        i += 1
-    return float(diffs)/float(len(s1))
-
-"""
-Computes the JC distance between two sequences.
-Args:
-    s1: string 1
-    s2: string 2 
-Returns:
-    Throws error if the strings are not of the same length
-    Else, computes JC distance
-"""
-
-def JC_distance(s1,s2):
-    prop_diff = prop_diff(s1,s2)
-    return 1 - (np.log(1 - 4/3*prop_diff))
-
 
 def closest_neighbors(M):
     size = len(M)
