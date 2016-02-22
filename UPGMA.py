221b3ddf35450988e64a2693f0e1df78267890db
apearson1995 <apearson1995@berkeley.edu>
2016-02-22 14:24:54 -0800

    Moved Project stuff into an python notebook
    Moved Project functions into the ipython notebook for project things

--- UPGMA.py
+++ UPGMA.py
@@ -5,8 +5,74 @@
 #          Bidit Acharya
 #          Tracy Lou
 
+import matplotlib.pyplot as plt
 import numpy as np
+import math
 from ete3 import Tree
+
+
+"""
+Constructs a Jukes Cantor transition Matrix with a specified alpha level a
+Args:
+     a: alpha level for the Jukes Cantor Matrix
+Returns:
+     Transition Matrix corresponding to the Jukes-Cantor Algorithm
+"""
+
+def JC_matrix(a):
+
+    """
+    >>> np.trace(JC_matrix(.25))
+    3.0
+    """
+
+    b = a/3
+    M = np.array([[1-a, b, b, b],
+                 [b, 1-a, b, b],
+                 [b, b, 1-a, b],
+                 [b, b, b, 1-a]])
+    return M
+
+
+"""
+Computes proportion of differing letters from two strings of the same size
+Args:
+    s1: string 1
+    s2: string 2 
+Returns:
+    Throws error if the strings are not of the same length
+    Else, returns proportion (in between 0 and 1) of differing letters
+"""
+
+def prop_diff(s1,s2):
+    # """ LOOK curious as to why this isn't working
+    # >>> prop_diff("ATTGAC","ATGGCC") 
+    # float(2)/float(6)  
+    # """
+    if len(s1) != len(s2):
+        raise ValueError("Cannot compute compare DNA sequences of differing lenth")
+    diffs = 0
+    i     = 0
+    while i < len(s1):
+        if s1[i] != s2[i]:
+            diffs += 1
+        i += 1
+    return float(diffs)/float(len(s1))
+
+"""
+Computes the JC distance between two sequences.
+Args:
+    s1: string 1
+    s2: string 2 
+Returns:
+    Throws error if the strings are not of the same length
+    Else, computes JC distance
+"""
+
+def JC_distance(s1,s2):
+    prop_diff = prop_diff(s1,s2)
+    return 1 - (np.log(1 - 4/3*prop_diff))
+
 
 def closest_neighbors(M):
     size = len(M)
@@ -18,8 +84,13 @@ def closest_neighbors(M):
                 min = M[i,j]
                 coordinates = (i,j)
     return coordinates
+
 """
-please note that taxa1 must be less than taxa2 numerically
+Computes the subsequent Distance Matrix of the UPGMA algorithm
+Args:
+    M: the transition Matrix for the Jukes Cantor Algorithm
+Returns:
+    subsequent matrix using UPGMA
 """
 def update_UPGMA_matrix(M, taxa1, taxa2):
         size  = len(M)
@@ -47,15 +118,12 @@ def update_UPGMA_matrix(M, taxa1, taxa2):
                 ans[0,new_col] = (M[min(taxa1, j), max(taxa1, j)] + M[min(taxa2, j), max(taxa2, j)]) / 2
                 new_col += 1
         return ans
-"""
-Computes the subsequent Distance Matrix of the UPGMA algorithm
-Args:
-    M: the transition Matrix for the Jukes Cantor Algorithm
-Returns:
-    subsequent matrix using UPGMA
-"""
 
 def UPGMA(M, names):
+    """
+    >>> UPGMA(np.array([[0, .45, .27, .53],[0,   0, .40, .50],[0,   0,   0, .62],[0,   0,   0,  0]]))
+    np.array([[0, .425, .575],[0,    0,  .50],[0,    0,    0]])
+    """
     def search_nodes(trees ,name):
         for tree in trees:
             if tree.name == name:
@@ -101,6 +169,14 @@ def UPGMA(M, names):
             break
         M = update_UPGMA_matrix(M, taxa1, taxa2)
     return trees[0]
+
+"""
+Work-Horse function of UPGMA Algorithm, that given an array of sequences
+returns a phylogenetic tree of distances represented as a list of lists
+"""
+
+def JC_UPGMA(M):
+    return "Need to implement this function"
 
 
 
