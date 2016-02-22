07126237719b21311e4dda46d7062ad44df70c16
Joshua P Nixon <nixonpjoshua@gmail.com>
2016-02-21 19:02:40 -0800

    better testing

--- UPGMAtest.py
+++ UPGMAtest.py
@@ -1,4 +1,5 @@
 from UPGMA import *
+from ete3 import Tree
 import numpy as np
 a = UPGMA(np.array([[0, .45, .27, .53],[0,   0, .40, .50],[0,   0,   0, .62],[0,   0,   0,  0]]), ['a', 'b', 'c', 'd'])
-print(str(a) + '\n\n')
+a.show()
