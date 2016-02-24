6577423f7e6a2ebf614e64cba5f1755429d029e4
Joshua P Nixon <nixonpjoshua@gmail.com>
2016-02-23 17:26:49 -0800

    fully joined the two distance methods

--- distance_based_trees.py
+++ distance_based_trees.py
@@ -8,6 +8,16 @@
 import numpy as np
 from ete3 import Tree
 
+def sums_others(M):
+    size = len(M)
+    sums = np.zeros(size)
+    for i in xrange(size):
+        s = 0
+        for other in xrange(size):
+            s += M[min(other, i), max(other, i)]
+        sums[i] = s
+    return sums
+
 def closest_neighbors(M):
     size = len(M)
     min = 100000
@@ -20,18 +30,44 @@ def closest_neighbors(M):
     return coordinates
 
 """
+Makes Q matrix that decides what will be joined 
+Args:
+    M: symetric matrix
+Returns:
+    Q matrix
+"""
+def make_Q_matrix(M, sums):
+    N  = M.shape[0]        # number of taxa
+    Q  = np.zeros(M.shape) # matrix to be returned 
+    for i in xrange(N):
+        for j in xrange(N):
+            if i < j:
+                Q[i][j] = (N-2)*M[i][j] - sums[i] - sums[j]
+    return Q
+
+"""
 Computes the subsequent Distance Matrix of the UPGMA algorithm
 Args:
     M: the transition Matrix for the Jukes Cantor Algorithm
 Returns:
     subsequent matrix using UPGMA
 """
-def UPGMA_new_dist(M, taxa1, taxa2, i, j):
+def UPGMA_new_dist(M, taxa1, taxa2, j):
     return (M[min(taxa1, j), max(taxa1, j)] + M[min(taxa2, j), max(taxa2, j)]) / 2
+
+def neighbor_joining_new_dist(M, taxa1, taxa2, j):
+    return (M[min(taxa1, j), max(taxa1, j)] + M[min(taxa2, j), max(taxa2, j)] - M[taxa1, taxa2])/2.0
 
 def split_dist(M, taxa1, taxa2):
     avg_dist = M[taxa1, taxa2]/2
     return (avg_dist, avg_dist)
+
+# TODO this means we compute sums others twice per loop
+def neighbor_joining_parent_dist(M, taxa1, taxa2):
+    sums = sums_others(M)
+    taxa1_dist = M[taxa1, taxa2]/2.0 + (sums[taxa1] - sums[taxa2])/(2*(len(M)-2))
+    return (taxa1_dist, M[taxa1, taxa2] - taxa1_dist)
+    
 
 def update_matrix(M, taxa1, taxa2, new_dist_fn):
         size  = len(M)
@@ -55,22 +91,18 @@ def update_matrix(M, taxa1, taxa2, new_dist_fn):
         for j in xrange(size):
             if j != taxa1 and j != taxa2:
                 #exploits the fact that we have an upper triangular so col > row always
-                ans[0,new_col] = UPGMA_new_dist(M, taxa1, taxa2, i, j)
+                ans[0,new_col] = UPGMA_new_dist(M, taxa1, taxa2, j)
                 new_col += 1
         return ans
 
-def neighbor_based_method(M, names, new_dist_fn, parent_dist_fn):
-    """
-    >>> UPGMA(np.array([[0, .45, .27, .53],[0,   0, .40, .50],[0,   0,   0, .62],[0,   0,   0,  0]]))
-    np.array([[0, .425, .575],[0,    0,  .50],[0,    0,    0]])
-    """
+def neighbor_based_method(M, names, closest_neighbors_fn new_dist_fn, parent_dist_fn):
     def search_nodes(trees ,name):
         for tree in trees:
             if tree.name == name:
                 return tree
     trees = []
     while True:
-        taxa1, taxa2 = closest_neighbors(M)
+        taxa1, taxa2 = closest_neighbors_fn(M)
         if taxa1 > taxa2:
             tmp = taxa1
             taxa1 = taxa2
@@ -109,11 +141,10 @@ def neighbor_based_method(M, names, new_dist_fn, parent_dist_fn):
     return trees[0]
 
 def UPGMA(M, names):
-    return neighbor_based_method(M, names, UPGMA_new_dist, avg)
+    return neighbor_based_method(M, names, closest_neighbors UPGMA_new_dist, split_dist)
 
-
-
-
+def neighbor_joining(M, names):
+    return neighbor_based_method(M, names, lambda m : closest_neighbors(make_Q_matrix(m)), neighbor_joining_new_dist, neighbor_joining_parent_dist)
 
 
 
