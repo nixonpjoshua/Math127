a2fc3f0c51cb96a6b49b580516d6ea6cbc9e4978
apearson1995 <apearson1995@berkeley.edu>
2016-02-23 17:02:46 -0800

    mutate_tester

---
+++ mut_tester.py
@@ -0,0 +1,29 @@
+# Group 6: mutate function
+
+# Authors: Josh Nixon
+#          Alex Pearson
+#          Bidit Acharya
+#          Tracy Lou
+
+import numpy as np
+from numpy import linalg as LA
+from mutate import *
+
+
+print(tree_simulator(.3,4,'GATTACA'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
