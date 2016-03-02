711b729b25d2dccd7b830a0599e68c2ed936eeb3
apearson1995 <apearson1995@berkeley.edu>
2016-03-01 16:56:49 -0800

    Fixed bug in tests the Algorithms work
    Fixed Bug in Test

--- distance_based_trees.test.py
+++ distance_based_trees.test.py
@@ -2,42 +2,64 @@ from distance_based_trees import *
 from ete3 import Tree
 import numpy as np
 
-a = UPGMA(np.array([[0, .45, .27, .53],
-					[0,   0, .40, .50],
-					[0,   0,   0, .62],
-					[0,   0,   0,  0]]), 
-					['a', 'b', 'c', 'd'])
-print(a)
+from distance_based_trees import *
+from ete3 import Tree
+import numpy as np
 
-# http://www.southampton.ac.uk/~re1u06/teaching/upgma/upgma15.PNG
-
-b = UPGMA(np.array([[0, 19.0,  27.0,  8.0, 33.0, 18.0 ,13.0],
+M = np.array([[0, 19.0,  27.0,  8.0, 33.0, 18.0 ,13.0],
 					[0,    0,  31.0, 18.0, 36.0,  1.0, 13.0],
 					[0,    0,   0,   26.0, 41.0, 32.0, 29.0],
 					[0,    0,   0,      0, 31.0, 17.0, 14.0],
 					[0,    0,   0,      0,    0, 35.0, 28.0],
-					[0,    0,   0,      0,    0,    0, 12.0]]), 
-					['a', 'b', 'c', 'd','e','f','g'])
+					[0,    0,   0,      0,    0,    0, 12.0],
+			        [0,    0,   0,      0,    0,    0,   0 ]
+			  ])
 
-print(b)
+names = ['a', 'b', 'c', 'd','e','f','g']
 
 
-a = neighbor_joining(np.array([[0, .45, .27, .53],
-					[0,   0, .40, .50],
-					[0,   0,   0, .62],
-					[0,   0,   0,  0]]), 
-					['a', 'b', 'c', 'd'])
-print(a)
+ans = neighbor_based_method(M,names, closest_neighbors, UPGMA_new_dist, split_dist)
+print(ans)
 
-# http://www.southampton.ac.uk/~re1u06/teaching/upgma/upgma15.PNG
-
+# a = UPGMA(np.array([[0, .45, .27, .53],
+# 					[0,   0, .40, .50],
+# 					[0,   0,   0, .62],
+# 					[0,   0,   0,  0]]),
+# 					['a', 'b', 'c', 'd'])
+# print(a)
+#
+# # http://www.southampton.ac.uk/~re1u06/teaching/upgma/upgma15.PNG
+#
+# b = UPGMA(np.array([[0, 19.0,  27.0,  8.0, 33.0, 18.0 ,13.0],
+# 					[0,    0,  31.0, 18.0, 36.0,  1.0, 13.0],
+# 					[0,    0,   0,   26.0, 41.0, 32.0, 29.0],
+# 					[0,    0,   0,      0, 31.0, 17.0, 14.0],
+# 					[0,    0,   0,      0,    0, 35.0, 28.0],
+# 					[0,    0,   0,      0,    0,    0, 12.0]]),
+# 					['a', 'b', 'c', 'd','e','f','g'])
+#
+# print(b)
+#
+#
+# a = neighbor_joining(np.array([[0, .45, .27, .53],
+# 					[0,   0, .40, .50],
+# 					[0,   0,   0, .62],
+# 					[0,   0,   0,  0]]),
+# 					['a', 'b', 'c', 'd'])
+# print(a)
+#
+# # http://www.southampton.ac.uk/~re1u06/teaching/upgma/upgma15.PNG
+#
 b = neighbor_joining(np.array([[0, 19.0,  27.0,  8.0, 33.0, 18.0 ,13.0],
 					[0,    0,  31.0, 18.0, 36.0,  1.0, 13.0],
 					[0,    0,   0,   26.0, 41.0, 32.0, 29.0],
 					[0,    0,   0,      0, 31.0, 17.0, 14.0],
 					[0,    0,   0,      0,    0, 35.0, 28.0],
-					[0,    0,   0,      0,    0,    0, 12.0]]), 
+					[0,    0,   0,      0,    0,    0, 12.0],
+					[0,    0,   0,      0,    0,    0, 12.0]
+					]),
 					['a', 'b', 'c', 'd','e','f','g'])
 
 print(b)
+
 
