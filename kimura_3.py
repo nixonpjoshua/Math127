a52f11ccca1b8b34b00d31c182bef2fd39095096
tlouskt <t.lou1742@gmail.com>
2016-03-06 15:44:08 -0800

    finished, gotta check

--- kimura_3.py
+++ kimura_3.py
@@ -14,10 +14,10 @@ Return:
 
 def K3_matrix(a, b):
 		
-	M = np.array([1-a-2b, a, b, g],
-				[a, 1-a-2b, g, b]
-				[b, g, 1-a-2b, a]
-				[g, b, a, 1-a-2b])
+	M = np.array([1-a-b-g, a, b, g],
+				[a, 1-a-b-g, g, b]
+				[b, g, 1-a-b-g, a]
+				[g, b, a, 1-a-b-g])
 	return M
 
 """
@@ -48,17 +48,22 @@ count_trv1 = 0
 count_trv2 = 0
 count_trs = 0
 
-transversions = ['AC', 'CA', 'AT', 'TA', 'CG', 'GC', 'GT', 'TG']
+transversions_b = ['AC', 'CA','GT', 'TG']
+transversions_g = ['AT', 'TA', 'CG', 'GC']
 transitions = ['AG', 'GA', 'CT', 'TC']
 
 for i, j in s1 and s2:
-	if i + j in transversions:
-		count_trv += 1
+	if i + j in transversions_b:
+		count_trv1 += 1
+	if i + j in transversions_g:
+		count_trv2 += 1
 	if i + j in transitions:
 		count_trs += 1
 
 prop_diff_a = float(count_trs) / float(len(s1))
-prop_diff_b = float(count_trv) / float(len(s1))
+prop_diff_b = float(count_trv1) / float(len(s1))
+prop_diff_g = float(count_trv2) / float(len(s1)
+
 
 """
 Computes the K3 distance between the two sequences.
@@ -69,7 +74,14 @@ Args:
 
 Returns:
 	Error if the sequences are not of the same length
-	Else, computes the K2 distance
+	Else, computes the K3 distance
 """
 
 def K3_distance(s1, s2):
+	return -0.25((np.log(1-2*prop_diff_a - 2*prop_diff_b) 
+			+ np.log(1-2*prop_diff_b - 2*prop_diff_g) + 
+			(np.log(1 - 2*prop_diff_b - 2*prop_diff_g))
+
+
+
+
