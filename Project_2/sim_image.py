9e3dd5159c12bfb63cd075fac98719426ba18bc3
apearson1995 <apearson1995@berkeley.edu>
2016-04-18 16:49:32 -0700

    Separated File into  Alex's Approach and Josh's approach
    Also created a function that gives a centered coord system at (0,0,0)
    rather than corner at (0,0,0)

--- Project_2/sim_image.py
+++ Project_2/sim_image.py
@@ -1,15 +1,71 @@
 import numpy as np 
 
 """
+Goal
+
 1.  Compute 3D DFT of molecule mol, producing F(mol) another 3D Array
 2.  Restrict F(mol) to the image plane P_f thus producing a 2D Array
 2a. Interpolation buisness that I don't quite understand how to code up
+"""
+
+## Alex's Approach
+def alex_project_fst(mol, R):
+	nice        = np.fft.fftn(mol) 
+	restriction = restriction(mol,R)
+	return np.fft.ifftn(np.fft.fftn(mol))
+
+"""
+Creates a 2D Array that represents a restriction of mol.
+Args: 
+	mol: an NxNXN matrix that contains voxels of electro potential
+	R: 3x3 rotation matrix 
+Returns:
+	Returns a restriction of mol onto the image plane
+Note:
+	Interpolation is required for this step
+"""
+def restriction(mol,R):
+	return #2Darray
+
+
+"""
+Creates function that transforms old coordinates into 0 centered
+coordinates.
+Args: 
+	mol: an NxNXN matrix that contains voxels of electro potential
+Returns:
+	Function that given a list of 3 corrdinates transforms them into 
+	coordinate system centered at 0.
+
+"""
+def center_maker(mol):
+	# Assumes Cube input
+	N = mol.shape[0] - 1 # size - 1
+	# old list is a list of the original coordinates
+	def center(old_list):
+		mid  = N/2
+		new  = [0,0,0]
+		for i in np.arange(len(old_list)):
+			old = old_list[i]
+			dist = abs(old-mid)
+			if old == mid:
+				new[i] = 0
+			elif old < mid:
+				new[i] = -1*dist
+			else:
+				new[i] = dist
+		return new
+	return center
+
+
 
 
 """
 
+"""
 
-def project_fst(mol, R):
+## Josh's Approach
+def josh_project_fst(mol, R):
 	nice = np.fft.fftn(mol)
 	def transform(x, y):
 		# use R to convert to i, j, k from x, y
@@ -22,19 +78,7 @@ def project_fst(mol, R):
 	return output
 
 
-"""
-Questions:
 
-What type of Discrete Fourier Transform of mol are we computing?
-Since we are given a 3D array mol I am pretty sure we are computing
-3 dimenstional fourier transform so I looked at:
-
-http://docs.scipy.org/doc/numpy/reference/generated/numpy.fft.fftn.html#numpy.fft.fftn
-
-"""
-
-def compute_3D_DFT(mol):
-	return
 
 
 
--- Project_2/sim_image.test.py
+++ Project_2/sim_image.test.py
@@ -1,14 +1,62 @@
 import numpy as np 
 from MRCFile import MRCFile
 
-zika_153 = MRCFile('zika_153.mrc')
+"""
+Playing around with the data adnd the syntax
+"""
+
+zika_153 = MRCFile('zika_153.mrc') # initializing object
 zika_153.load_all_slices()
-print(type(zika_153))
+mol = zika_153.slices
+
+# print(type(mol))
+
+# print(mol)
+
+# Testing Whether centering the image function changes the fast fourier transform
+# 3x3x3 array
+
+test = np.array([[[ 0,  1,  2],
+        [ 3,  4,  5],
+        [ 6,  7,  8]],
+       [[ 9, 10, 11],
+        [12, 13, 14],
+        [15, 16, 17]],
+       [[18, 19, 20],
+        [21, 22, 23],
+        [24, 25, 26]]])
+
+print("the bottom slice is", test[0])
+print("-------------------------------")
+print("the middle slice is", test[1])
+print("--------------------------------")
+print("the top slice is", test[2])
+
+"""
+Playing around with utility functions
+"""
+def center_maker(mol):
+	# Assumes Cube input
+	N = mol.shape[0] - 1 # size - 1
+	# old list is a list of the original coordinates
+	def center(old_list):
+		mid  = N/2
+		new  = [0,0,0]
+		for i in np.arange(len(old_list)):
+			old = old_list[i]
+			dist = abs(old-mid)
+			if old == mid:
+				new[i] = 0
+			elif old < mid:
+				new[i] = -1*dist
+			else:
+				new[i] = dist
+		return new
+	return center
 
 
-
-
-
+center_zika = center_maker(mol)
+print(center_zika([0,0,0]))
 
 
 
