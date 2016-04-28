98aa3573557d22ce0acb59e54517adb8bb05c6b7
apearson1995 <apearson1995@berkeley.edu>
2016-04-28 13:59:23 -0700

    Reorganized files and created rotation matrix generators
    For mathematical justification i.e. why these are rotation matrices
    look up https://en.wikipedia.org/wiki/Rotation_matrix

---
+++ Project_2/back_projection.test.py
@@ -0,0 +1,148 @@
+import numpy as np
+from MRCFile import MRCFile
+from sim_image import *
+import matplotlib.pyplot as plt
+import matplotlib.image
+import sys
+
+
+# Cooking up our own set of rotation matrices
+# For more information see 
+# https://en.wikipedia.org/wiki/Rotation_matrix
+
+
+"""
+Cooks up "evenly spaced" rotation matrices that rotate 
+the yz plane about the x direction
+
+Args: 
+    n: number of rotation matrices 
+     
+Returns:
+    list of rotation matrices
+"""
+
+def rotate_x(n):
+	# range of angle is from 0 to pi
+	step = np.pi/n
+	# + 1e-5 hacky fix to make range inclusive
+	thetas = np.arange(0,np.pi + 1e-5 ,step) 
+	# TODO fix for efficieny purposes
+	matrices = []
+	for j in np.arange(len(thetas)):
+		i = thetas[j]
+		new_matrix = np.array([[ 1, 0, 0],
+							   [ 0 , np.cos(i), -np.sin(i)],
+							   [ 0 , np.sin(i), np.cos(i)]
+							   ])
+		matrices.append(new_matrix)
+	return matrices
+
+"""
+Cooks up "evenly spaceed" rotation matrices that rotate 
+the xz plane about the y direction
+
+Args: 
+    n: number of rotation matrices 
+     
+Returns:
+    np array of rotation matrices
+"""
+def rotate_y(n):
+	# range of angle is from 0 to pi
+	step = np.pi/n
+	# + 1e-5 hacky fix to make range inclusive
+	thetas = np.arange(0,np.pi + 1e-5 ,step) 
+	# TODO fix for efficieny purposes
+	matrices = []
+	for j in np.arange(len(thetas)):
+		i = thetas[j]
+		new_matrix = np.array([[ np.cos(i), 0, np.sin(i)],
+							   [ 0 , 1, 0],
+							   [ -np.sin(i) , 0, np.cos(i)]
+							   ])
+		matrices.append(new_matrix)
+	return matrices
+
+"""
+Cooks up "evenly spaceed" rotation matrices that rotate 
+the xz plane about the y direction
+
+Args: 
+    n: number of rotation matrices 
+     
+Returns:
+    np array of rotation matrices
+"""
+def rotate_y(n):
+	# range of angle is from 0 to pi
+	step = np.pi/n
+	# + 1e-5 hacky fix to make range inclusive
+	thetas = np.arange(0,np.pi + 1e-5 ,step) 
+	# TODO fix for efficieny purposes
+	matrices = []
+	for j in np.arange(len(thetas)):
+		i = thetas[j]
+		new_matrix = np.array([[ np.cos(i), -np.sin(i), 0],
+							   [ np.sin(i) , np.cos(i), 0],
+							   [ 0 , 0, 1]
+							   ])
+		matrices.append(new_matrix)
+	return matrices
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
