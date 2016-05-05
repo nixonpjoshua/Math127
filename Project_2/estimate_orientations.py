87c2048ff669bf10d1603b2948d2da25dbc69b73
apearson1995 <apearson1995@berkeley.edu>
2016-05-04 21:17:05 -0700

    Slight changes

--- Project_2/estimate_orientations.py
+++ Project_2/estimate_orientations.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def estimate_3(im1,im2,im3):
 	"""
-	Gives Coordinate frames corresponding to three different directions
+	Computes Coordinate frames corresponding to three different directions
 	used as a helper function for estimate_orientations
 
 	Args: 
@@ -20,63 +20,43 @@ def estimate_3(im1,im2,im3):
 # Meat of argument goes here i.e. how to find rotation matrices #####
 #####################################################################
 
-def compute_common_lines(im1,im2,im3):
+def get_common(im1,im2):
 	"""
-	Gives Coordinate frames corresponding to three different directions
-	used as a helper function for estimate_orientations
+	Computes common line between 2 imagesas expressed by vector of values
+
+	Args: 
+    	im1: A 2d array representing image 1
+    	im2: A 2d array representing image 2
+
+    Returns:
+    	A vector with array 
+	"""
+
+	return
+
+
+def get_common_lines(im1,im2,im3):
+	"""
+	Computes common lines as expressed by array of values
 
 	Args: 
     	im1: A 2d array representing image 1
     	im2: A 2d array representing image 2
     	im3: A 2d array representing image 3
 
-    Returns:
-    	# TODO: Figure out what data structure lines should be 
-    	# represented as does it have to be values in the array?
-	"""
-	return
-
-def compute_common_lines(im1,im2):
-	"""
-	Gives Coordinate frames corresponding to three different directions
-	used as a helper function for estimate_orientations
-
-	Args: 
-    	im1: A 2d array representing image 1
-    	im2: A 2d array representing image 2
 
     Returns:
-    	# TODO: Figure out what data structure lines should be 
-    	# represented as does it have to be values in the array?
+    	A list with commmon lines ordered s.t.
+    	[cl_(1,2) , cl_(1,3) , cl_(2,3) ]
 	"""
-	return
-
-
-# Leave all of the trig to Prof. Dynerman
-
-#
-# Am confused from here on as to how to "set" the first rotaion frame at 
-# F_1 = np.array([[1,0,0],
-#				  [0,1,0],
-#				  [0,0,1]
-#				])
-#
+	cl_12 = get_common(im1,im2)
+	cl_13 = get_common(im1,im3)
+	cl_23 = get_common(im2,im3)
+	return [cl_12, cl_13, cl_23]
 
 #####################################################################
-# Uses estimate_3 successively to find rotation matrices and then ###
-# usesback projection to try and find rho                         ###
+###       Dynerman said he would give us most of the rest         ###
 #####################################################################
-
-def estimate_orientations():
-	"""
-	"""
-	return 
-
-
-
-
-
-
 
 
 
