a7ecdce28228a2f7518f430f4195266e196081cc
Joshua P Nixon <nixonpjoshua@gmail.com>
2016-05-05 17:39:19 -0700

    closer to notes from OH
    we belive we should have sinc(z*D) in OH it was sinc(z*D*pi) but numpy has pi in thier sinc function

--- Project_2/back_projection.py
+++ Project_2/back_projection.py
@@ -23,7 +23,7 @@ def compute_b(I):
     # equal to D
     fourier_image = np.fft.fft2(I)
     z = np.arange(len_I)
-    sinc_terms = np.complex128(np.sinc(len_I*z)*len_I).reshape(len_I, 1, 1)
+    sinc_terms = np.complex128(np.sinc(len_I*z)).reshape(len_I, 1, 1)
     # TODO note that now at z[0] it evaluates to len_I, not sure which is correct should revisit equation
 
     # z_list = []
