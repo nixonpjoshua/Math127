675f9dd72fd63dbda9a64e822f50505e99de7353
Joshua P Nixon <nixonpjoshua@gmail.com>
2016-05-07 18:31:21 -0700

    improvements to testing

--- Project_2/test.py
+++ Project_2/test.py
@@ -4,24 +4,40 @@ from sim_image import project_fst
 from MRCFile import MRCFile
 import numpy as np
 import sys
-def rotate_x(n):
+def rotate(n, axis):
     thetas = np.linspace(0, np.pi, n)
     matrices = []
     for j in np.arange(len(thetas)):
         i = thetas[j]
-        new_matrix = np.array([[ 1, 0, 0],
-                               [ 0 , np.cos(i), -np.sin(i)],
-                               [ 0 , np.sin(i), np.cos(i)]
+        if axis == 'x':
+            new_matrix = np.array([[ 1, 0, 0],
+                                   [ 0 , np.cos(i), -np.sin(i)],
+                                   [ 0 , np.sin(i), np.cos(i)]
+                                   ])
+        if axis == 'y':
+            new_matrix = np.array([[ np.cos(i), 0, np.sin(i)],
+                               [ 0 , 1, 0],
+                               [ -np.sin(i) , 0, np.cos(i)]
+                               ])
+        if axis == 'z':
+            new_matrix = np.array([[ np.cos(i), -np.sin(i), 0],
+                               [ np.sin(i) , np.cos(i), 0],
+                               [ 0 , 0, 1]
                                ])
         matrices.append(new_matrix)
-    return np.array(matrices)
+    return matrices
+
+
+def evenly_rotate(n):
+    return rotate(n/3, 'x') + rotate(n/3, 'y') + rotate(n/3, 'z')
+
 if len(sys.argv) != 2:
-    print "please call with an mrc file as argument"
+    print "please call with an mrc file as argument 1, and number of rotations as argument 2"
 else:
     f = MRCFile(sys.argv[1])
     f.load_all_slices()
     images = []
-    Rs = rotate_x(4)
+    Rs = evenly_rotate(sys.argv[2])
     # TODO here we can make many images later
     for R in Rs:
         images.append(project_fst(f.slices, R))
@@ -31,6 +47,6 @@ else:
     # print "\norientations\n"
     # print orientations
     f.slices = back_project(images, Rs)
-    f.write_file('true_R.mrc')
+    f.write_file('true_R.mrc', overwrite=True)
     # f.slices = back_project(images, orientations)
     # f.write_file('estimated_R.mrc')
