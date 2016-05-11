8da9beba2a1fa145c8872a948c9a0b9e0288bfb4
Joshua P Nixon <nixonpjoshua@gmail.com>
2016-05-10 22:57:58 -0700

    cleanse of old files

--- Project_2/testing.py
+++
@@ -1,7 +0,0 @@
-from MRCFile import MRCFile
-import numpy as np
-f = MRCFile('zika_153.mrc')
-f.load_all_slices()
-f.write_file('zika_update1.mrc')
-f.slices = np.zeros(f.slices.shape)
-f.write_file('zika_update2.mrc')
